2a31459667c9275e8436e882ac81e451964933d4
mauve <ranger@mauve.moe>
2020-05-05 11:50:34 -0400

    Made query easier to read for tests

--- add-on/index.js
+++ add-on/index.js
@@ -1,9 +1,9 @@
 const mdns = require('multicast-dns')()
 
-const QUERY_INTERVAL = 2000
-const QUERY_NAME = 'ipfs.local'
+const QUERY_INTERVAL = 10000
+const QUERY_NAME = Math.floor(Math.random() * 255) + `.example.local`
 
-mdns.on('query', (query) => console.log({ query }))
+mdns.on('query', ({questions}) => console.log(questions[0]))
 
 function doQuery () {
   mdns.query({
